685aa75f2514b2c283079404191ee6619e46d668
TSUJI Mariko <mtsuji@student.42.fr>
2025-08-21 11:50:35 +0200

    Add deployment configurations for app1 and app3, create services for both apps, and set up ingress rules for routing. Update deploy script to include new services and ingress deployment.

--- p2/scripts/deploy.sh
+++ p2/scripts/deploy.sh
@@ -2,3 +2,15 @@
 
 echo "Deploying yml file !"
 kubectl apply -f /vagrant/confs/deployment.yml
+
+echo "Deploying app1 service !"
+kubectl apply -f /vagrant/confs/app1/service.yml
+
+echo "Deploying app2 service !"
+kubectl apply -f /vagrant/confs/app2/service.yml
+
+echo "Deploying app3 service !"
+kubectl apply -f /vagrant/confs/app3/service.yml
+
+echo "Deploying ingress !"
+kubectl apply -f /vagrant/confs/ingress.yml
